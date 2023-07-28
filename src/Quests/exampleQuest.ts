a76fc5dc7d87abe332ddc064faa30ce0a7f93d02
ggaiti <ggaiti@hotmail.com>
2023-07-28 12:28:51 -0400

    quest function

--- src/Quests/exampleQuest.ts
+++ src/Quests/exampleQuest.ts
@@ -6,7 +6,8 @@ interface player {
   quest: object[];
 }
 
-const quest = {
+const quest1 = {
+  name: "example quest",
   progress: 0,
   story: (progress: Number) => exampleStory(progress),
 };
@@ -32,27 +33,60 @@ const exampleStory = async (progress: Number) => {
       ]);
       console.log(response.choice);
       if (response.choice === 0) {
-        quest.progress = 1;
-        pushQuest();
+        pushQuest(player, quest1);
+        updateCurrentQuest(quest1);
       } else {
-        pushQuest();
-        return;
+        console.log("come back later");
       }
-
+      break;
     default:
       break;
   }
+  //   console.log(player);
 };
 let player: player = {
   name: "ALEX",
   quest: [],
 };
-const pushQuest = () => {
-  let playerCopy = { ...player };
-  playerCopy.quest.push(quest);
-  console.log(playerCopy.quest);
+const pushQuest = (player: Object, quest: Object) => {
+  let questCopy = {
+    ...quest,
+    name: { ...quest.name },
+    progress: { ...quest.progress },
+    story: { ...quest.story },
+  };
+  let playerCopy = { ...player, quest: [...player.quest] };
+  if (player.quest === true) {
+    return player;
+  } else {
+    playerCopy.quest.push(questCopy);
+  }
+  player = playerCopy;
+  quest = questCopy;
+  console.log(player);
 };
-// pushQuest();
-quest.story(0);
 
+//function that does both functions
+const updateCurrentQuest = (quest) => {
+  let copyQuest = { ...quest, questProgress: { ...quest.progress } };
+  copyQuest.progress += 1;
+  quest = copyQuest;
+};
+
+const questComplete = (quest) => {
+  let questCopy = {
+    ...quest,
+    name: { ...quest.name },
+    progress: { ...quest.progress },
+  };
+  if (quest.progress > exampleStory.progress) {
+    quest.progress += 1;
+  } else {
+    console.log("quest complete");
+  }
+  quest = questCopy;
+};
+
+// pushQuest();
+quest1.story(0);
 //update quest,
